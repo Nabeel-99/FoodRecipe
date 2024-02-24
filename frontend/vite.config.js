74d165f242b9fdc1784178e64f98245a02273f43
Farouk Nabil <nabeel@Farouks-Air.home>
2024-02-24 21:02:18 +0100

    fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0'
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
+  }
+  
 })
